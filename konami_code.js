e9a3f9749095e63b1851a07baa8bcabb7458780b
Michelle B <mjblevins7@gmail.com>
2017-12-23 22:29:21 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,7 +5,7 @@ let index = 0;
 function init() {
   // Write your JavaScript code inside the init() function
 
-const body = document.getElementByTagName('body')
+const body = document.getElementsByTagName('body');
 
 body.addEventListener('keydown', function onKeyDownHandler(e) {
   const key = parseInt(e.detail || e.which || e.location);
