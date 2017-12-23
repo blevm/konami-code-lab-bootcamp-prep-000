622b01491e9b0a59d92f676817d1d414c2bc7c08
Michelle B <mjblevins7@gmail.com>
2017-12-23 22:26:19 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -9,13 +9,13 @@ const body = document.getElementByTag('body')
 
 body.addEventListener('keydown', function onKeyDownHandler(e) {
   const key = parseInt(e.detail || e.which || e.location);
- 
+
   if (key === code[index]) {
     index++;
- 
+
     if (index === code.length) {
       alert("Hurray!");
- 
+
       index = 0;
     }
   } else {
@@ -24,5 +24,5 @@ body.addEventListener('keydown', function onKeyDownHandler(e) {
 })
 
 
-  
+
 }
