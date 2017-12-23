8d12624e6a2c2c853a4cedd7632ef1ca16aeb734
Michelle B <mjblevins7@gmail.com>
2017-12-23 22:43:34 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,16 +2,6 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let index = 0;
 
-function init() {
-  // Write your JavaScript code inside the init() function
-
-const body = document.getElementsByTagName('body');
-
-body.addEventListener('keydown', function(){
-  onKeyDownHandler(event);
-}, true);
-
-}
 
 function onKeyDownHandler(e) {
   const key = parseInt(e.detail || e.which || e.location);
@@ -28,3 +18,13 @@ function onKeyDownHandler(e) {
     index = 0;
   }
 };
+
+
+function init() {
+  // Write your JavaScript code inside the init() function
+
+const body = document.getElementsByTagName('body');
+
+body.addEventListener('keydown', onKeyDownHandler, true);
+
+}
